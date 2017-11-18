5d8acde41286204278120f8e86349c636163ab4c
stevenkoontz <31934289+stevenkoontz@users.noreply.github.com>
2017-11-18 11:43:56 -0500

    Create Plot1.R

---
+++ Plot1.R
@@ -0,0 +1,12 @@
+#Load Data and Make Dataframe
+housepower <- read.table("household_power_consumption.txt", sep = ";", na.strings = "?", header = TRUE)
+housepower$Date <- as.Date(housepower$Date, format="%d/%m/%Y")
+febdata <- subset(housepower, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(housepower)
+datetime <- paste(as.Date(febdata$Date), febdata$Time)
+febdata$Datetime <- as.POSIXct(datetime)
+
+#Plot1
+hist(febdata$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+dev.copy(png, file ="Plot1.png", height = 480, width = 480)
+dev.off()

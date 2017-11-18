5a4db6611a74fa3f21cfdf10b7e482522d9e0393
stevenkoontz <31934289+stevenkoontz@users.noreply.github.com>
2017-11-18 12:03:48 -0500

    Update Plot4.R

--- Plot4.R
+++ Plot4.R
@@ -6,14 +6,24 @@ rm(housepower)
 datetime <- paste(as.Date(febdata$Date), febdata$Time)
 febdata$Datetime <- as.POSIXct(datetime)
 
-#Plot4
+#Set the Graphic Parameter
 par(mfrow=c(2,2), mar=c(4,4,2,1))
+
+#Make the first Plot
 plot(febdata$Global_active_power~febdata$Datetime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+
+#Make the second Plot
 plot(febdata$Voltage~febdata$Datetime, type="l", ylab="Voltage", xlab="datetime")
+
+#Make the third Plot
 plot(febdata$Datetime, febdata$Sub_metering_1, type="l", ylab="Energy sub metering", xlab="")
 lines(febdata$Datetime, febdata$Sub_metering_2, col="red")
 lines(febdata$Datetime, febdata$Sub_metering_3, col="blue")
 legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), bty = "n")
+
+#Make the fourth Plot
 plot(febdata$Global_reactive_power~febdata$Datetime, type="l", ylab="Global_reactive_power", xlab="datetime")
+
+#copy Plot4 to PNG
 dev.copy(png, file ="Plot4.png", height = 480, width = 480)
 dev.off()
